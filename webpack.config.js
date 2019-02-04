1db2b07877db6dd1fd8edd7fde432bd3efa2cf65
Andrei Chaplygin <gotacoin@gmail.com>
2019-02-04 02:51:34 -0800

    Added webpack and SCSS support

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: './app/scripts/app.ts',
+    devtool: 'inline-source-map',
+    module: {        
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }, 
+            {
+                test: /\.(png|svg|jpg|gif|ico|svg)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: './img'
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                  "style-loader",
+                  MiniCssExtractPlugin.loader,
+                  "css-loader",
+                  "sass-loader"
+                ]
+              }
+        ]
+    },
+    devServer: {
+        contentBase: './dist',
+        port: 5000,
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new CopyWebpackPlugin([
+            { from: './app/img/*', to: './img', flatten: true }
+          ]),
+        new MiniCssExtractPlugin({
+            filename: "./styles/[name].[contenthash].css"
+          }),
+        new HtmlWebpackPlugin({
+            hash: true,    
+            template: './app/index.html',
+            filename: './index.html'
+        })
+    ]
+};
